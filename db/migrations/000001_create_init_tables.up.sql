e4442455f0459b5fa565a520a1a31cb971b9d40c
Dika <dikamaah@gmail.com>
2024-06-13 18:10:29 +0700

    feat: api update status appointment

--- db/migrations/000001_create_init_tables.up.sql
+++ db/migrations/000001_create_init_tables.up.sql
@@ -56,3 +56,128 @@ CREATE TABLE IF NOT EXISTS fundus_feedbacks (
     CONSTRAINT fk_fundus_id FOREIGN KEY (fundus_id) REFERENCES funduses(id),
     CONSTRAINT fk_doctor_id FOREIGN KEY (doctor_id) REFERENCES users(id)
 );
+
+CREATE TABLE IF NOT EXISTS doctor_profiles {
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    specialization VARCHAR(255) NOT NULL,
+    str_number VARCHAR(255) NOT NULL,
+    bio TEXT DEFAULT '',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+};
+
+CREATE TABLE IF NOT EXISTS doctor_availabilities {
+    id SERIAL PRIMARY KEY,
+    doctor_id INTEGER NOT NULL,
+    day_of_week INTEGER NOT NULL,
+    start_hour TIME NOT NULL,
+    end_hour TIME NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_doctor_id FOREIGN KEY (doctor_id) REFERENCES users(id)
+};
+
+CREATE TABLE IF NOT EXISTS doctor_practices {
+    id SERIAL PRIMARY KEY,
+    doctor_id INTEGER NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    province VARCHAR(255) NOT NULL,
+    office_name VARCHAR(255) NOT NULL,
+    address TEXT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+};
+
+CREATE TABLE IF NOT EXISTS doctor_educations {
+    id SERIAL PRIMARY KEY,
+    doctor_id INTEGER NOT NULL,
+    degree VARCHAR(255) NOT NULL,
+    school_name VARCHAR(255) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_doctor_id FOREIGN KEY (doctor_id) REFERENCES users(id)
+};
+
+CREATE TABLE IF NOT EXISTS appointments {
+    id SERIAL PRIMARY KEY,
+    patient_id INTEGER NOT NULL,
+    doctor_id INTEGER NOT NULL,
+    date DATE NOT NULL,
+    start_hour TIME NOT NULL,
+    end_hour TIME NOT NULL,
+    status VARCHAR(255) DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_patient_id FOREIGN KEY (patient_id) REFERENCES users(id),
+    CONSTRAINT fk_doctor_id FOREIGN KEY (doctor_id) REFERENCES users(id)
+};
+
+CREATE TABLE IF NOT EXISTS health_facilities {
+    id SERIAL PRIMARY KEY,
+    facility_name VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    province VARCHAR(255) NOT NULL,
+    address TEXT NOT NULL,
+    adaptor_quantity INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+};
+
+CREATE TABLE IF NOT EXISTS adaptors {
+    id SERIAL PRIMARY KEY,
+    facility_id INTEGER NOT NULL,
+    device_code VARCHAR(255) NOT NULL,
+    used BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_facility_id FOREIGN KEY (facility_id) REFERENCES health_facilities(id)
+};
+
+CREATE TABLE IF NOT EXISTS user_adaptors {
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    adaptor_id INTEGER NOT NULL,
+    date DATE NOT NULL,
+    start_hour TIME NOT NULL,
+    end_hour TIME NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+};
+
+CREATE TABLE chat_rooms (
+    id SERIAL PRIMARY KEY,
+    doctor_id INTEGER NOT NULL,
+    patient_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_doctor_id FOREIGN KEY (doctor_id) REFERENCES users(id),
+    CONSTRAINT fk_patient_id FOREIGN KEY (patient_id) REFERENCES users(id)
+);
+
+CREATE TABLE chat_messages (
+    id SERIAL PRIMARY KEY,
+    chat_room_id INTEGER NOT NULL,
+    sender_id INTEGER NOT NULL,
+    message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_chat_room_id FOREIGN KEY (chat_room_id) REFERENCES chat_rooms(id),
+    CONSTRAINT fk_sender_id FOREIGN KEY (sender_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS notifications {
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    message TEXT NOT NULL,
+    is_read BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+};
